e1458956046364d291c4ff86a353aec1fd7e16a9
bhagyaravi <bhagyaravi98@gmail.com>
2020-07-18 19:06:30 +0530

    Update ExampleResource.java

--- src/main/java/org/acme/ExampleResource.java
+++ src/main/java/org/acme/ExampleResource.java
@@ -13,4 +13,16 @@ public class ExampleResource {
     public String hello() {
         return "hello";
     }
+    @POST
+	@Transactional
+	@Path("/submitpayment")
+	@Consumes(MediaType.APPLICATION_JSON)
+	Response makePayment(Payment payment) {
+		Calendar calobj = Calendar.getInstance();
+		DateFormat df = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
+		if (df.format(payment.getExpiryDate()).compareTo(df.format(calobj.getTime())) > 0)
+			return Response.status(500).entity("Card has expired").build();
+		payment.persist();
+		return Response.ok(201).entity(payment).build();		
+	}
 }
