7a1562f12f91e336b578bb1e8f7f9dd8a4cf69d9
bhagyaravi <bhagyaravi98@gmail.com>
2020-07-18 19:21:06 +0530

    Update ExampleResource.java

--- src/main/java/org/acme/ExampleResource.java
+++ src/main/java/org/acme/ExampleResource.java
@@ -4,6 +4,11 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.transaction.Transactional;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.core.Response;
+
 
 @Path("/hello")
 public class ExampleResource {
