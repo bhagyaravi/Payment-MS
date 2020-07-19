f563a92e3e67fd5c039f57efac3016ff1bda07a7
bhagyaravi <bhagyaravi98@gmail.com>
2020-07-19 06:42:41 +0530

    Update ExampleResource.java

--- src/main/java/org/acme/ExampleResource.java
+++ src/main/java/org/acme/ExampleResource.java
@@ -14,6 +14,7 @@ import java.text.DateFormat;
 import java.util.Calendar;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import javax.transaction.Transactional;
 
 import com.test.quarkus.model.Payment;
 
